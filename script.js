3620d3d0272879c449562984e975050542f76685
Grant Greenberg <19313835+grantgreenberg@users.noreply.github.com>
2023-12-14 10:33:38 -0700

    feat: basic working game strucure using factory functions and IIFE.

--- script.js
+++ script.js
@@ -13,12 +13,47 @@ const gameBoard = (function() {
     const placeMarker = (index, marker) => {
         if(checkEmptyCells()[index]) {
       		board[index] = marker;
+            return true;
         }
+        return false
     }
 
     let boardState = () => board;
 
-    return { placeMarker, boardState, checkEmptyCells }
+    return { placeMarker, boardState }
 
 })();
 
+function gamePlay() {
+
+    const player1 = createPlayer('Player One', 'X');
+    const player2 = createPlayer('Player Two', 'O');
+
+    let activePlayer = player1;
+
+    const togglePlayer = () => activePlayer == player1 ? activePlayer = player2 : activePlayer = player1;
+    
+    const playRound = (index) => {
+
+		if(gameBoard.placeMarker(index, activePlayer.marker)) {
+            togglePlayer();
+            console.log(`It's ${activePlayer.name}'s turn!`);
+            console.log(gameBoard.boardState());
+        }
+        else {
+            console.log('Invalid move. Please select another cell index.');
+        }
+
+        checkWinner();
+    }
+
+    return { playRound };
+}
+
+const game = gamePlay();
+
+game.playRound(0);
+game.playRound(0);
+game.playRound(1);
+game.playRound(1);
+game.playRound(2);
